6dee593c2df249c74b03ef068224266d4060f828
Fauszt Andras <fauszt.andras@nixs.com>
2023-02-08 19:40:05 +0100

    RANDOMIZER

--- pages/api/move.js
+++ pages/api/move.js
@@ -15,11 +15,12 @@ export default function handler(req, res) {
     "player": "F"
 }`
 
-    const response = JSON.parse(r);
+    // const response = JSON.parse(req.body.board ?? '[]');
 
-    const transpose = R.transpose(req.board)
+    const transpose = R.transpose(req.body.board)
     const legal = transpose.map((e, index) => ({row:e.length - 1 - e.reverse().findIndex(i => i === '_'), col:index}))
     const filtered = legal.filter(e=>e.row < 6);
+    console.log(filtered[Math.floor(Math.random() * filtered.length)])
     const move = filtered[Math.floor(Math.random() * filtered.length)].col
 
     res.status(200).json({column:move})
