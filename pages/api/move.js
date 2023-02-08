a97166787e144a48167db63b8c6fe3f9fc2adb24
Fauszt Andras <fauszt.andras@nixs.com>
2023-02-08 18:57:20 +0100

    move

--- pages/api/move.js
+++ pages/api/move.js
@@ -1,3 +1,24 @@
+import * as R from 'ramda';
+
 export default function handler(req, res) {
-  res.status(200).json({ "column": 1 })
+    let r = `
+  {
+    "gameId": "12345",
+    "board": [
+    ["_","_","_","_","_","_","_","_","_"],
+    ["_","_","_","x","_","_","_","_","_"],
+    ["_","_","_","x","_","_","_","_","_"],
+    ["_","_","_","x","_","_","_","_","_"],
+    ["S","F","_","x","_","F","_","_","_"],
+    ["F","S","S","x","_","F","F","S","S"]
+    ],
+    "player": "F"
+}`
+
+    const response = JSON.parse(r);
+
+    let t = R.transpose(response.board)
+    let legal = t.map(e=>e.length - e.reverse().findIndex(i=>i === '_'))
+
+    res.status(200).json(legal)
 }
