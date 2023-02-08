0af3bc48060bd5ac0449a5aab2887c0570ba5c8d
Fauszt Andras <fauszt.andras@nixs.com>
2023-02-08 20:46:56 +0100

    Revert "chore: nextTable"
    This reverts commit eab3ed09129b725dd8acc7442b8937d5a5520d0a.

--- pages/api/move.js
+++ pages/api/move.js
@@ -1,26 +1,28 @@
 import * as R from 'ramda';
 import didPlayerWin from '../../functions/didPlayerWin';
-import nextTable from '../../utils/nextTable';
 
 export default function handler(req, res) {
-  const transpose = R.transpose(req.body.board);
-  const legal = transpose.map((e, index) => ({
-    row: e.length - 1 - e.reverse().findIndex((i) => i === '_'),
-    col: index,
-  }));
-  const filtered = legal.filter((e) => e.row < 6);
-  console.log(filtered[Math.floor(Math.random() * filtered.length)]);
-  const move = filtered[Math.floor(Math.random() * filtered.length)].col;
+    let r = `
+  {
+    "gameId": "12345",
+    "board": [
+    ["S","_","_","_","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
+    ["S","F","_","x","_","F","_","_","_"],
+    ["F","S","S","x","_","F","F","S","S"]
+    ],
+    "player": "F"
+}`
 
-  res.status(200).json({ column: move });
+    // const response = JSON.parse(req.body.board ?? '[]');
 
-  const response = JSON.parse(req.body.board);
+    const transpose = R.transpose(req.body.board)
+    const legal = transpose.map((e, index) => ({row:e.length - 1 - e.reverse().findIndex(i => i === '_'), col:index}))
+    const filtered = legal.filter(e=>e.row < 6);
+    console.log(filtered[Math.floor(Math.random() * filtered.length)])
+    const move = filtered[Math.floor(Math.random() * filtered.length)].col
 
-  let t = R.transpose(response.board);
-
-  const nextSteps = legal.map((item, index) =>
-    nextTable(t, index, 6 - item.row, response.player)
-  );
-
-  res.status(200).json(nextSteps);
+    res.status(200).json({column:move})
 }
--- utils/nextTable.js
+++
@@ -1,8 +0,0 @@
-import { cloneDeep } from 'lodash';
-
-export default function nextTable(currentTable, row, column, player) {
-  const nextTable = cloneDeep(currentTable);
-
-  nextTable[row][column] = player;
-  return nextTable;
-}
