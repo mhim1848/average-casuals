83120021b589a8e9b221747d52dd05b9c62c0459
Fauszt Andras <fauszt.andras@nixs.com>
2023-02-08 20:47:36 +0100

    RANDOMIZER

--- pages/api/move.js
+++ pages/api/move.js
@@ -1,28 +1,10 @@
 import * as R from 'ramda';
-import didPlayerWin from '../../functions/didPlayerWin';
 
 export default function handler(req, res) {
-    let r = `
-  {
-    "gameId": "12345",
-    "board": [
-    ["S","_","_","_","_","_","_","_","_"],
-    ["S","_","_","x","_","_","_","_","_"],
-    ["S","_","_","x","_","_","_","_","_"],
-    ["S","_","_","x","_","_","_","_","_"],
-    ["S","F","_","x","_","F","_","_","_"],
-    ["F","S","S","x","_","F","F","S","S"]
-    ],
-    "player": "F"
-}`
-
-    // const response = JSON.parse(req.body.board ?? '[]');
-
     const transpose = R.transpose(req.body.board)
-    const legal = transpose.map((e, index) => ({row:e.length - 1 - e.reverse().findIndex(i => i === '_'), col:index}))
-    const filtered = legal.filter(e=>e.row < 6);
-    console.log(filtered[Math.floor(Math.random() * filtered.length)])
+    const legal = transpose.map((e, index) => ({row: e.length - 1 - e.reverse().findIndex(i => i === '_'), col: index}))
+    const filtered = legal.filter(e => e.row < 6);
     const move = filtered[Math.floor(Math.random() * filtered.length)].col
 
-    res.status(200).json({column:move})
+    res.status(200).json({column: move})
 }
