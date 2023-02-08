b7eb0eed72676e821399e294dfefc7cb6098fead
Fauszt Andras <fauszt.andras@nixs.com>
2023-02-08 19:24:43 +0100

    RANDOMIZER

--- pages/api/move.js
+++ pages/api/move.js
@@ -5,10 +5,10 @@ export default function handler(req, res) {
   {
     "gameId": "12345",
     "board": [
-    ["_","_","_","_","_","_","_","_","_"],
-    ["_","_","_","x","_","_","_","_","_"],
-    ["_","_","_","x","_","_","_","_","_"],
-    ["_","_","_","x","_","_","_","_","_"],
+    ["S","_","_","_","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
+    ["S","_","_","x","_","_","_","_","_"],
     ["S","F","_","x","_","F","_","_","_"],
     ["F","S","S","x","_","F","F","S","S"]
     ],
@@ -17,8 +17,10 @@ export default function handler(req, res) {
 
     const response = JSON.parse(r);
 
-    let t = R.transpose(response.board)
-    let legal = t.map(e=>e.length - e.reverse().findIndex(i=>i === '_'))
+    const transpose = R.transpose(req.board)
+    const legal = transpose.map((e, index) => ({row:e.length - 1 - e.reverse().findIndex(i => i === '_'), col:index}))
+    const filtered = legal.filter(e=>e.row < 6);
+    const move = filtered[Math.floor(Math.random() * filtered.length)].col
 
-    res.status(200).json(legal)
+    res.status(200).json(move)
 }
